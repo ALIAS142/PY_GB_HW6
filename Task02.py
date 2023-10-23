837fa113ad13058d13df89da7e8950d7726a1ed3
Anna <kochurovaav@gmail.com>
2023-10-24 02:29:01 +0300

    Task02 is modified

--- Task02.py
+++ Task02.py
@@ -5,12 +5,21 @@
 # 15
 # Вывод: [1, 9, 13, 14, 19]
 
-list = [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
+# list = [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
+#
+# min_number = int(input("Entrh min:  "))
+# max_number = int(input("Entrh max:  "))
+#
+# for i in range(len(list)):
+#     if min_number <= list[i] <= max_number:
+#
+#      print(i)
 
-min_number = int(input("Entrh min:  "))
-max_number = int(input("Entrh max:  "))
+list_1 = [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
 
-for i in range(len(list)):
-    if min_number <= list[i] <= max_number:
+min_number = 0
+max_number = 10
 
-     print(i)
+for i in range(len(list_1)):
+    if min_number <= list_1[i] <= max_number:
+        print(i, end=' ')
